6dc71827e8f4997b44b4c9838844b664b3323ffa
João Victor Reis Alves <62678763+JoaoReisA@users.noreply.github.com>
2022-05-11 21:55:36 -0300

    Add atividade 12

---
+++ Atividade12/atividade12.js
@@ -0,0 +1,13 @@
+
+function onHover(){
+    document.querySelector("img").src = "janelaFechada.jpg"
+}
+
+function onClick() {
+    document.querySelector("img").src = "janelaQuebrada.jpg"
+}
+
+function onMouseOut() {
+    document.querySelector("img").src = "janelaAberta.jpg"
+}
+
